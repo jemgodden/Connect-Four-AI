28f90ba0b0bac9256e8c06f2b141b1f0a8f30f9e
jemgodden <jemgodden@gmail.com>
2022-02-21 21:50:36 +0000

    Added minimum column agent.

--- connectx/agents.py
+++ connectx/agents.py
@@ -9,14 +9,21 @@ class Agent:
         """
         self.board: Board = board
 
-
-class RandomAgent(Agent):
     def performTurn(self) -> int:
         """
-        Agent performs turn by selecting column to drop counter into.
+        Agent performs turn by selecting a random non-full column to drop a counter into.
         :return: Int column in which the counter will be dropped.
         """
         return random.choice([i + 1 for i in range(self.board.cols) if self.board.colCounter(i) != self.board.rows])
+
+
+class MinimumAgent(Agent):
+    def performTurn(self) -> int:
+        """
+        Agent performs turn by selecting non-full column with minimum value to drop a counter into.
+        :return: Int column in which the counter will be dropped.
+        """
+        return min([i + 1 for i in range(self.board.cols) if self.board.colCounter(i) != self.board.rows])
 
 
 # class PPOAgent(Agent):
